fd79e46c20d135dcef4ff9e7cf30645fa13a469d
Dragonxjanith <101912503+Dragonxjanith@users.noreply.github.com>
2022-03-19 20:56:48 -1200

    updated Bot 🚀

--- plugins/office.js
+++ plugins/office.js
@@ -12,7 +12,7 @@ if (Config.WORKTYPE == 'public') {
             var image = await axios.get ('https://telegra.ph/file/011d37f1cef06440daf7a.jpg', {responseType: 'arraybuffer'})
 
         await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `╭───────────────
-│ ⎝🛡️ SOFTWARE STORE BY ACHI BRO LK 🛡️⎠
+│ ⎝🛡️ SOFTWARE STORE BY Janith sadanwan 🛡️⎠
 │      Ms Office Softwares 
 │  
 │  
