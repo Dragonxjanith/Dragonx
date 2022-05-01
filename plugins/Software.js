49fd7c6183a4d8eb0f59829aa2d34804c538b5cf
Dragonxjanith <101912503+Dragonxjanith@users.noreply.github.com>
2022-04-30 23:46:37 -1200

    Updated 🇱🇰 𝗗𝗥𝗔𝗚𝗢𝗡 𝗫  𝗪𝗛𝗔𝗧𝗦𝗔𝗣𝗣 𝗕𝗢𝗧 🕊

--- plugins/Software.js
+++ plugins/Software.js
@@ -9,10 +9,10 @@ if (Config.WORKTYPE == 'public') {
 
     Trex.addrex({pattern: 'softstore', fromMe: false,  deleteCommand: true }, (async (message, match) => {
 
-            var image = await axios.get ('https://telegra.ph/file/24429ad5b37c11525894e.jpg', {responseType: 'arraybuffer'})
+            var image = await axios.get ('https://telegra.ph/file/35883180004518cb15fe9.jpg', {responseType: 'arraybuffer'})
 
         await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `╭───────────────
-│  ⎝🛡️ SOFTWARE STORE BY ACHI BRO LK 🛡️⎠
+│  🇱🇰 🕊 SOFTWARE STORE BY ᴊᴀɴɪᴛʜ ꜱᴀᴅᴀɴᴜᴡᴀɴ 🇱🇰 🇱🇰 🕊
 │
 │  ◎Adobe Editing Softwares type =.adobe
 │  ◎Wondershare Softwares type =.ws
@@ -21,8 +21,8 @@ if (Config.WORKTYPE == 'public') {
 │  ◎Office Softwares type =.office   
 │  ◎Android Emulator Form Pc =.em
 │
-│         🛡️POWERD BY🛡️
-│        ⎝🛡️  ᗪᖇ𝙰𝙶𝚘𝚗 Ӽ   🛡️⎠
+│         🇱🇰 🕊POWERD BY🇱🇰 🕊
+│        🇱🇰 🕊  ᗪᖇ𝙰𝙶𝚘𝚗 Ӽ   🇱🇰 🕊
 ╰──────────────`,quoted: message.data})
 
     }));
@@ -34,7 +34,7 @@ if (Config.WORKTYPE == 'public') {
             var image = await axios.get ('https://telegra.ph/file/4fb6db2465bed4af3eb51.jpg', {responseType: 'arraybuffer'})
 
         await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `╭───────────────
-│  ⎝🛡️ SOFTWARE STORE BY ACHI BRO LK 🛡️⎠
+│  🇱🇰 🕊 SOFTWARE STORE BY ᴊᴀɴɪᴛʜ ꜱᴀᴅᴀɴᴜᴡᴀɴ 🇱🇰 🇱🇰 🕊
 │    adobe editing softwares
 │    
 │  ◎Adobe Photoshop =.aps
@@ -43,8 +43,8 @@ if (Config.WORKTYPE == 'public') {
 │  ◎Adobe Ligthroom =.alm
 │   
 │
-│         🛡️POWERD BY🛡️
-│        ⎝🛡️  ᗪᖇ𝙰𝙶𝚘𝚗 Ӽ   🛡️⎠
+│         🇱🇰 🕊POWERD BY🇱🇰 🕊
+│        🇱🇰 🕊  ᗪᖇ𝙰𝙶𝚘𝚗 Ӽ   🇱🇰 🕊
 ╰──────────────`,quoted: message.data})
 
 
@@ -56,7 +56,7 @@ if (Config.WORKTYPE == 'public') {
 
             var image = await axios.get ('https://telegra.ph/file/4b7a5d02fba1b2e147f41.jpg', {responseType: 'arraybuffer'})
         await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `╭───────────────
-│  ⎝🛡️ SOFTWARE STORE BY ACHI BRO LK 🛡️⎠
+│  🇱🇰 🕊 SOFTWARE STORE BY ᴊᴀɴɪᴛʜ ꜱᴀᴅᴀɴᴜᴡᴀɴ 🇱🇰 🇱🇰 🕊
 │         Operating System
 │
 │  ◎Windwos 7 =.win7
@@ -64,8 +64,8 @@ if (Config.WORKTYPE == 'public') {
 │  ◎Windwos 10 pro =.win10
 │  ◎Windwso 11 pro =.win11
 │
-│         🛡️POWERD BY🛡️
-│        ⎝🛡️  ᗪᖇ𝙰𝙶𝚘𝚗 Ӽ   🛡️⎠
+│         🇱🇰 🕊POWERD BY🇱🇰 🕊
+│        🇱🇰 🕊  ᗪᖇ𝙰𝙶𝚘𝚗 Ӽ   🇱🇰 🕊
 ╰──────────────`,quoted: message.data})
 
   }));
@@ -75,15 +75,15 @@ if (Config.WORKTYPE == 'public') {
 
             var image = await axios.get ('https://telegra.ph/file/4d6dc262fedc23e93cc69.jpg', {responseType: 'arraybuffer'})
         await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `╭───────────────
-│  ⎝🛡️ SOFTWARE STORE BY ACHI BRO LK 🛡️⎠
+│  🇱🇰 🕊 SOFTWARE STORE BY ᴊᴀɴɪᴛʜ ꜱᴀᴅᴀɴᴜᴡᴀɴ 🇱🇰 🇱🇰 🕊
 │       Android Emulator From Pc
 │  
 │   ◎Blustaucks 5 =.bl
 │   ◎Nox player =.nx
 │   ◎Memu player =.mm
 │   
-│         🛡️POWERD BY🛡️
-│         ⎝🛡️  ᗪᖇ𝙰𝙶𝚘𝚗 Ӽ   🛡️⎠
+│         🇱🇰 🕊POWERD BY🇱🇰 🕊
+│         🇱🇰 🕊  ᗪᖇ𝙰𝙶𝚘𝚗 Ӽ   🇱🇰 🕊
 ╰──────────────`,quoted: message.data})
 
     }));
@@ -93,7 +93,7 @@ if (Config.WORKTYPE == 'public') {
 
             var image = await axios.get ('https://telegra.ph/file/44f514afcbb5953196b92.jpg', {responseType: 'arraybuffer'})
         await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: `╭───────────────
-│  ⎝🛡️ SOFTWARE STORE BY ACHI BRO LK 🛡️⎠
+│  🇱🇰 🕊 SOFTWARE STORE BY ᴊᴀɴɪᴛʜ ꜱᴀᴅᴀɴᴜᴡᴀɴ 🇱🇰 🇱🇰 🕊
 │        Antivirus Softwares
 │  ◎Avira Antivirus Pro 15.0.29 =.aa
 │  ◎IObit Advanced SystemCare Pro 12.4.0 =.as1
@@ -101,8 +101,8 @@ if (Config.WORKTYPE == 'public') {
 │  ◎Avast Premium Security 20.10.5824 =.is2 
 │  ◎Advanced SystemCare Ultimate 13.5.0.173 =.as2
 │
-│         🛡️POWERD BY🛡️
-│         ⎝🛡️  ᗪᖇ𝙰𝙶𝚘𝚗 Ӽ   🛡️⎠
+│         🇱🇰 🕊POWERD BY🇱🇰 🕊
+│         🇱🇰 🕊  ᗪᖇ𝙰𝙶𝚘𝚗 Ӽ   🇱🇰 🕊
 ╰──────────────`,quoted: message.data})
 
     }));
