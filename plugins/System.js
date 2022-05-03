1e0bd9531e8909d4890464aec3f0edd28356b67d
Dragonxjanith <101912503+Dragonxjanith@users.noreply.github.com>
2022-05-02 15:17:26 -1200

    updated

--- plugins/System.js
+++ plugins/System.js
@@ -3,69 +3,175 @@
 const Trex = require('../events');
 const {MessageType, MessageOptions, Mimetype, Presence} = require('@adiwajshing/baileys');
 const {spawnSync} = require('child_process');
+const Config = require('../config');
 const chalk = require('chalk');
 const axios = require('axios');
-const fs = require('fs');
-let Work_Mode = Build.WORKTYPE == 'public' ? false : true
 
 const Language = require('../language');
 const Lang = Language.getString('system_stats');
- 
-var SYSDTXT = ''
-if (Build.LANG == 'SI') SYSDTXT = '💻 පද්ධති තත්ත්වය'
-if (Build.LANG == 'EN') SYSDTXT = '💻 System status'
-
-var VER = ''
-if (Build.LANG == 'SI') VER = '🧬 Version'
-if (Build.LANG == 'EN') VER = '🧬 Version'
-
-var MSG = ''
-if (Build.ALIVEMSG == 'default') MSG = '```Hey There! Bot Online now. 💃🏻♥️```\n\n*Developer:* ```Black Amda```\n\n*Official Website:* https://amdaniwasa.com\n\n*Youtube channel :* https://www.youtube.com/channel/UCZx8U1EU95-Wn9mH4dn15vQ\n\n```Thank You For Using Queen Amdi💞```'
-else MSG = Build.ALIVEMSG
 
 
-Trex.addrex({pattern: 'alive', fromMe: Work_Mode, desc: Lang.ALIVE_DESC,  deleteCommand: false}, (async (message, match) => {
-     
-    var logo = await axios.get (Build.ALIVE_LOGO, {responseType: 'arraybuffer'})
-    var PIC = Buffer.from(logo.data)
+if (Config.WORKTYPE == 'private') {
 
-    const media = await message.client.prepareMessage(message.jid, PIC, MessageType.image, { thumbnail: PIC })
+    Trex.addrex({pattern: 'alive', fromMe: true,  deleteCommand: false,  desc: Lang.ALIVE_DESC}, (async (message, match) => {
 
-    var BUTTHANDLE = '';
-    if (/\[(\W*)\]/.test(Build.HANDLERS)) {
-        BUTTHANDLE = Build.HANDLERS.match(/\[(\W*)\]/)[1][0];
-    } else {
-        BUTTHANDLE = '.';
+        if (Config.ALIVEMSG == 'default') {
+            
+            const buttons = [
+  {buttonId: 'MENU', buttonText: {displayText: '🤟 I AM FINE 😜'}, type: 1},//jakakkak
+  {buttonId: 'MNU', buttonText: {displayText: '🥺 I  AM SAD 😥'}, type: 1},
+ ]
+
+const btn = {
+   // imageMessage: 'https://telegra.ph/file/35883180004518cb15fe9.jpg',
+    contentText: "♥️💫 HOW ARE YOU 🙈♥️",
+    footerText: 'BY ⎝🛡️ Dragon x 🛡️⎠ ▷',
+    buttons: buttons,
+    headerType: 1
+}
+
+            
+            var image = await axios.get ('https://telegra.ph/file/35883180004518cb15fe9.jpg', {responseType: 'arraybuffer'})
+       
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: "🇱🇰 Hay All I'm Alive now 🇱🇰\n\n🧿 My name : ⎝🛡️ Dragon x 🛡️⎠\n\n\n ◐. Developer:Janith Sadanuwan \n ◐. Github - https://tinyurl.com/ycq7ra4x \n ◐. Youtube - https://tinyurl.com/yalqofqs \n ◐. Owner -https://tinyurl.com/y7evah34  \n ◐. Our Web - https://tinyurl.com/y7ul7kt4 \n\n ◐. All Cammands -Type .menu\n\n\nThank You For Using ⎝🛡️ *Dragon x* 🛡️⎠"})
+            
+        await message.client.sendMessage (message.jid, btn, MessageType.buttonsMessage)
+
+
     }
-        
-    const buttons = [
-        {buttonId: BUTTHANDLE + 'qaversion', buttonText: {displayText: VER }, type: 1},
-        {buttonId: BUTTHANDLE + 'qasysstats', buttonText: {displayText: SYSDTXT }, type: 1}
-    ]
-    const buttonMessage = {
-        contentText: MSG,
-        footerText: 'ǫᴜᴇᴇɴ ᴀᴍᴅɪ © ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ',
-        buttons: buttons,
-        headerType: 4,
-        imageMessage: media.message.imageMessage    
-    }
-    await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage);
-}))
+    else {
+        const buttons = [
+  {buttonId: 'MENU', buttonText: {displayText: '🤟 I AM FINE 😜'}, type: 1},//jakakkak
+  {buttonId: 'MNU', buttonText: {displayText: '🥺 I  AM SAD 😥'}, type: 1},
+  ]
+const btn = {
+   // imageMessage: 'https://telegra.ph/file/35883180004518cb15fe9.jpg',
+    contentText: "♥️💫 HOW ARE YOU 🙈♥️",
+    footerText: 'BY ⎝🛡️ Dragon x 🛡️⎠ ▷',
+    buttons: buttons,
+    headerType: 1
+}
 
-Trex.addrex({pattern: 'qasysstats', fromMe: Work_Mode, desc: Lang.SYSD_DESC, dontAddCommandList: true,  deleteCommand: false}, (async (message, match) => {
-     
-   const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
-    await message.sendMessage(
-        '```' + child + '```', MessageType.text, {quoted: message.data}
-    );
-}));
 
-Trex.addrex({pattern: 'qaversion', fromMe: Work_Mode, desc: Lang.BOT_V, dontAddCommandList: true,  deleteCommand: false}, (async (message, match) => {
-    await message.client.sendMessage(message.jid, 
-        `*🧬 Queen Amdi Version 🧬*\n\n` + 
-        '```Installed version :```\n' +
-        Lang.version + 
-        `\n\nCheck official website : https://amdaniwasa.com/`
-   , MessageType.text, {quoted: message.data});
+
+            
+            var image = await axios.get ( Config.AL , {responseType: 'arraybuffer'})
+       
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.ALIVEMSG + '\n\n*⎝🛡️ Dragon x 🛡️⎠*' })
+        await message.client.sendMessage (message.jid, btn, MessageType.buttonsMessage)
+
+     }
+    }));
+
+    Trex.addrex({pattern: 'sysd', fromMe: true,  deleteCommand: false,  desc: Lang.SYSD_DESC, dontAddCommandList: true}, (async (message, match) => {
+
+        if (message.jid === '393475528094-1415817281@g.us') {
+
+            return;
+        }
+
+        const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
+        await message.sendMessage(
+            '```' + child + '```', MessageType.text
+        );
+    }));
+
+    Trex.addrex({pattern: 'version', fromMe: true,  deleteCommand: false,  desc: Lang.BOT_V}, (async (message, match) => {    
     
-}));
+        await message.client.sendMessage(message.jid, 
+                `*⎝🛡️ Dragon x 🛡️⎠*\n\n` + 
+                '```Installed version :```\n' +
+                ' V 1.0.0 Public'+
+                `\n\n🍁 Check github for bot: https://github.com/Janithsadanuwan/Dragonx-Whatsapp-Bot`
+           , MessageType.text);
+            
+        }));
+}
+else if (Config.WORKTYPE == 'public') {
+
+    Trex.addrex({pattern: 'alive', fromMe: false, desc: Lang.ALIVE_DESC}, (async (message, match) => {
+
+        if (Config.ALIVEMSG == 'default') {
+            
+            const buttons = [
+  {buttonId: 'MENU', buttonText: {displayText: '🙈 I AM FINE 😜'}, type: 1},//jakakkak
+  {buttonId: 'MNU', buttonText: {displayText: '🥺 I  AM SAD 😥'}, type: 1},
+ ]
+
+const btn = {
+   // imageMessage: 'https://telegra.ph/file/cd35928cef2be17d339c1.jpg',
+    contentText: "♥️💫 HOW ARE YOU 🙈♥️",
+    footerText: 'BY ⎝🛡️ Dragon x 🛡️⎠ ▷',
+    buttons: buttons,
+    headerType: 1
+}
+
+            
+            var image = await axios.get ('https://telegra.ph/file/35883180004518cb15fe9.jpg', {responseType: 'arraybuffer'})
+       
+
+       await message.client.updatePresence(message.jid,Presence.composing)
+
+        await new Promise(r => setTimeout(r, 1000));
+
+ 
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: "🇱🇰 Hay All I'm Alive now 🇱🇰\n\n🧿 My name : ⎝🛡️ Dragon x 🛡️⎠\n\n\n ◐. Developer:Janith Sadanuwan \n ◐. Github - https://tinyurl.com/ycq7ra4x \n ◐. Youtube - https://tinyurl.com/yalqofqs \n ◐. Owner -https://tinyurl.com/y7evah34  \n ◐. Our Web - https://tinyurl.com/y7ul7kt4 \n\n ◐. All Cammands -Type .menu\n\n\nThank You For Using ⎝🛡️ *Dragon x* 🛡️⎠"})
+            
+        await message.client.sendMessage (message.jid, btn, MessageType.buttonsMessage)
+
+
+    }
+    else {
+        const buttons = [
+  {buttonId: 'MENU', buttonText: {displayText: '🤟 I AM FINE 😜'}, type: 1},//jakakkak
+  {buttonId: 'MEN', buttonText: {displayText: '🥺 I  AM SAD 😥'}, type: 1},
+  ]
+const btn = {
+   // imageMessage: 'https://telegra.ph/file/35883180004518cb15fe9.jpg',
+    contentText: "♥️💫 HOW ARE YOU 🙈♥️",
+    footerText: 'BY ⎝🛡️ Dragon x 🛡️⎠ ▷',
+    buttons: buttons,
+    headerType: 1
+}
+
+
+
+            
+            var image = await axios.get ( Config.AL , {responseType: 'arraybuffer'})
+       
+
+       await message.client.updatePresence(message.jid,Presence.composing)
+
+        await new Promise(r => setTimeout(r, 1000));
+
+ 
+        await message.client.sendMessage (message.jid, Buffer.from (image.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.ALIVEMSG + '\n\n*⎝🛡️ Dragon x 🛡️⎠*' })
+        await message.client.sendMessage (message.jid, btn, MessageType.buttonsMessage)
+
+     }
+    }));
+
+    Trex.addrex({pattern: 'sysd', fromMe: true,  deleteCommand: false,  desc: Lang.SYSD_DESC, dontAddCommandList: true}, (async (message, match) => {
+
+        if (message.jid === '393475528094-1415817281@g.us') {
+
+            return;
+        }
+
+        const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
+        await message.sendMessage(
+            '```' + child + '```', MessageType.text
+        );
+    }));
+
+    Trex.addrex({pattern: 'version', fromMe: false, desc: Lang.BOT_V}, (async (message, match) => {    
+    
+        await message.client.sendMessage(message.jid, 
+                `*⎝🛡️ Dragon x 🛡️⎠*\n\n` + 
+                '```Installed version :```\n' +
+                ' V 1.0.0 - Public'+
+                `\n\n🍁 Check github for bot:  https://github.com/Janithsadanuwan/Dragonx-Whatsapp-Bot`
+           , MessageType.text);
+            
+        }));
+}
